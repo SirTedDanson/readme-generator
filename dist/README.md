8a982fa7129eabb5e7f017e7cbe275abe05b89ec
sirteddanson <knowledgeMED@gmail.com>
2022-01-07 18:27:41 -0500

    init readme population

--- dist/README.md
+++ dist/README.md
@@ -1,2 +1,49 @@
-# test
+
+# INTERPOLATE
+
+## Description 
+
+test 
+
+
+## Table of Contents
+
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+
+
+## Installation
+
+none
+
+
+## Usage 
+
+none 
+
+
+## License
+
+Mozilla Public License 2.0
+
+
+## Contributing
+
+Contributor Covenant
+
+## Tests
+
+none
+
+
+## Questions
+
+ted
+ted
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,21 +1,68 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (data) => {
   console.log(data);
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
-`;
+  ## Description 
+
+  ${data.description} 
+
+
+  ## Table of Contents
+
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+
+
+  ## Installation
+
+  ${data.installation}
+
+
+  ## Usage 
+
+  ${data.usage} 
+
+
+  ## License
+
+  ${data.license}
+
+
+  ## Contributing
+
+  ${data.contribute}
+
+  ## Tests
+
+  ${data.test}
+
+
+  ## Questions
+
+  ${data.github}
+  ${data.email}
+
+  `;
 }
 
 module.exports = generateMarkdown;
